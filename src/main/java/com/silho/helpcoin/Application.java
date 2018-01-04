84169af7f328fee09c2717c550683cae2b72e4ec
Samuel <samuel2629@gmail.com>
2018-01-04 14:50:35 +0100

    new v

--- src/main/java/com/silho/helpcoin/Application.java
+++ src/main/java/com/silho/helpcoin/Application.java
@@ -2,8 +2,10 @@ package com.silho.helpcoin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+@SpringBootApplication
 @EnableAutoConfiguration
 @ComponentScan
 public class Application {
